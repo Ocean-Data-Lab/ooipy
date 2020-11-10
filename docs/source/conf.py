df28df17b7d5e6c2518ee1ca087f0a2556675961
John-Ragland <jhrag@uw.edu>
2020-11-10 15:31:16 -0800

    updated conf.py for documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ release = '0.0.1'
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage','sphinx.ext.napoleon']
 
-autodoc_mock_imports = ['numpy', 'matplotlib', 'obspy', 'scipy', 'pandas', 'thredds_crawler', 'lxml', 'requests', 'seaborn', 'ffspec']
+autodoc_mock_imports = ['numpy', 'matplotlib', 'obspy', 'scipy', 'pandas', 'thredds_crawler', 'lxml', 'requests', 'seaborn', 'fsspec']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
