1e6f19035738c3dc3fa28a4fe32dc803a2e29885
FelixSchwock <fschwock@uw.edu>
2020-11-16 12:19:27 -0800

    documentation for ooiplotlib

--- ooipy/tools/ooiplotlib.py
+++ ooipy/tools/ooiplotlib.py
@@ -1,3 +1,9 @@
+'''
+This modules provides functions for plotting spectrograms and power
+spectral density estimates. It extends the matplotlib.pyplot.plot
+function.
+'''
+
 # Import all dependancies
 import datetime
 import numpy as np
@@ -11,8 +17,50 @@ from obspy.core import UTCDateTime
 
 def plot(*args, scalex=True, scaley=True, data=None, **kwargs):
     """
-    An extension to the matplotlib.pyplot.plot function that allows for the nice plotting of Spectrogram
-    and PSD objects. For a description of the input parameters, please refer to matplotlib.
+    An extension to the matplotlib.pyplot.plot function that allows for
+    the nice plotting of :calss:`~ooipy.hydrophone.basic.Spectrogram`
+    and :calss:`~ooipy.hydrophone.basic.Psd` objects. For a description
+    of the input parameters, please refer to the matplotlib
+    documentation.
+
+    Parameters
+    ----------
+    *args : :calss:`~ooipy.hydrophone.basic.Spectrogram`, or
+    :calss:`~ooipy.hydrophone.basic.Psd`, or array
+        object or array to be plotted
+    scalex :
+        see matplotlib documentation
+    scaley :
+        see matplotlib doccumentation
+    data :
+        see matplotlib doccumentation
+    **kwargs :
+        see matplotlib doccumentation,
+        :func:`~ooipy.tools.ooiplotlib.plot_spectrogram`, and
+        :func:`~ooipy.tools.ooiplotlib.plot_psd` for arguments
+
+    >>> from ooipy.request import hydrophone_request
+    >>> from ooipy.tools import ooiplotlib as ooiplt
+
+    >>> # pull OOI data and compute spectrogram and PSD
+    >>> start_time = datetime.datetime(2017,3,10,0,0,0)
+    >>> end_time = datetime.datetime(2017,3,10,0,5,0)
+    >>> node = '/PC01A'
+    >>> hydrophone_data = hydrophone_request.get_acoustic_data(
+            start_time, end_time, node)
+    >>> hydrophone_data.compute_spectrogram()
+    >>> hydrophone_data.compute_psd_welch()
+
+    >>> # plot spectrogram and change some default plot settings
+    >>> ooiplt.plot(hydrophone_data.spectrogram)
+    >>> plt.title('my spectrogram')
+    >>> plt.ylim([0, 20000])
+    >>> plt.show()
+
+    >>> # plot PSD and chnage some default plot settings
+    >>> ooiplt.plot(hydrophone_data.psd)
+    >>> plt.title('my PSD')
+    >>> plt.show()
     """
     for arg in args:
         if isinstance(arg, Spectrogram):
@@ -25,7 +73,48 @@ def plot(*args, scalex=True, scaley=True, data=None, **kwargs):
 
 def plot_spectrogram(spec_obj, **kwargs):
     """
-    Plot a spectrogram object using the matplotlib package. 
+    Plot a :calss:`~ooipy.hydrophone.basic.Spectrogram` object using the
+    matplotlib package.
+
+    Parameters
+    ----------
+    spec_obj : :calss:`~ooipy.hydrophone.basic.Spectrogram`
+        spectrogram object to be plotted
+    **kwargs :
+        See matplotlib doccumentation for list of arguments. Additional
+        arguments are:
+        * plot_spec : bool
+            If False, figure will be closed. Can save time if only
+            saving but not plotting is desired. Default is True
+        * save_spec : bool
+            If True, figure will be saved under **filename**. Default is
+            False
+        * filename : str
+            filename of figure if saved. Default is "spectrogram.png"
+        * xlabel_rot : int or float
+            rotation angle (deg) of x-labels. Default is 70
+        * fmin : int or float
+            minimum frequency. Default is 0
+        * fmax : int or float
+            maximum frequency. Default is 32000
+        * vmin : int or float
+            lower limit of level axis (colormap). Default is 20
+        * vmax : int or float
+            upper limit of level axis (colormap). Default is 80
+        * vdelta : int or float
+            resolution of level axis (colormap). Default is 1
+        * vdelta_cbar : int or float
+            label distance of colorbar. Default is 5
+        * figsize : (int, int)
+            width and height of figure, Default is (16, 9)
+        * res_reduction_time : int
+            reduction factor of time domain resolution. This can
+            facilitate faster plotting of large spectroagm objects.
+            Default is 1 (no reduction)
+        * res_reduction_freq : int
+            reduction factor of frequency domain resolution. This can
+            facilitate faster plotting of large spectroagm objects.
+            Default is 1 (no reduction)
     """
     # check for keys
     if 'plot_spec' not in kwargs: plot_spec = True
@@ -41,30 +130,34 @@ def plot_spectrogram(spec_obj, **kwargs):
     if 'vmax' not in kwargs: vmax = 80.0
     if 'vdelta' not in kwargs: vdelta = 1.0
     if 'vdelta_cbar' not in kwargs: vdelta_cbar = 5.0
-    if 'figsize' not in kwargs: figsize = (16,9)
+    if 'figsize' not in kwargs: figsize = (16, 9)
     if 'res_reduction_time' not in kwargs: res_reduction_time = 1
     if 'res_reduction_freq' not in kwargs: res_reduction_freq = 1
 
-    #set backend for plotting/saving:
+    # set backend for plotting/saving:
     if not plot_spec: matplotlib.use('Agg')
     font = {'size'   : 22}
     matplotlib.rc('font', **font)
 
+    # reduce resolution in time and frequency
     v = spec_obj.values[::res_reduction_time,::res_reduction_freq]
-
     if len(spec_obj.time) != len(spec_obj.values):
-        t = np.linspace(0, len(spec_obj.values) - 1, int(len(spec_obj.values) / res_reduction_time))
+        t = np.linspace(0, len(spec_obj.values) - 1,
+                        int(len(spec_obj.values) / res_reduction_time))
     else:
         t = spec_obj.time[::res_reduction_time]
-
     if len(spec_obj.freq) != len(spec_obj.values[0]):
-        f = np.linspace(0, len(spec_obj.values[0]) - 1, int(len(spec_obj.values[0]) / res_reduction_freq))
+        f = np.linspace(0, len(spec_obj.values[0]) - 1,
+                        int(len(spec_obj.values[0]) / res_reduction_freq))
     else:
         f = spec_obj.freq[::res_reduction_freq]
 
+    # plot spectrogram object
     cbarticks = np.arange(vmin,vmax+vdelta,vdelta)
     fig, ax = plt.subplots(figsize=figsize)
-    im = ax.contourf(t, f, np.transpose(v), cbarticks, norm=Normalize(vmin=vmin, vmax=vmax), cmap=plt.cm.jet, **kwargs)  
+    im = ax.contourf(t, f, np.transpose(v), cbarticks,
+                     norm=Normalize(vmin=vmin, vmax=vmax), cmap=plt.cm.jet,
+                     **kwargs)  
     plt.ylabel(ylabel)
     plt.xlabel(xlabel)
     plt.ylim([fmin, fmax])
@@ -82,28 +175,41 @@ def plot_spectrogram(spec_obj, **kwargs):
     if not plot_spec: plt.close(fig)
 
 def plot_psd(psd_obj, **kwargs):
-    '''
-    Plot a spectrogram object using the matplotlib package.
+    """
+    Plot a :calss:`~ooipy.hydrophone.basic.Psd` object using the
+    matplotlib package.
 
-    Basic visualization of PSD estimate based on matplotlib. The function offers two options: Plot PSD
-    in Python (plot_psd = True) and save PSD plot in directory (save_psd = True). PSDs are
-    plotted in dB re 1µ Pa^2/Hz.
-
-    plot_psd (bool): whether or not PSD is plotted using Python
-    save_psd (bool): whether or not PSD plot is saved
-    filename (str): directory where PSD plot is saved. Use ending ".png" or ".pdf" to save as PNG or PDF
-        file. This value will be ignored if save_psd=False
-    title (str): title of plot
-    ylabel (str): label of vertical axis
-    xlabel (str): label of horizontal axis
-    xlabel_rot (float): rotation of xlabel. This is useful if xlabel are longer strings.
-    fmin (float): minimum frequency (unit same as f) that is displayed
-    fmax (float): maximum frequency (unit same as f) that is displayed
-    vmin (float): minimum value (dB) of PSD.
-    vmax (float): maximum value (dB) of PSD.
-    figsize (tuple(int)): size of figure
-    dpi (int): dots per inch
-    '''
+    Parameters
+    ----------
+    spec_obj : :calss:`~ooipy.hydrophone.basic.Psd`
+        Psd object to be plotted
+    **kwargs :
+        See matplotlib doccumentation for list of arguments. Additional
+        arguments are:
+        * plot_spec : bool
+            If False, figure will be closed. Can save time if only
+            saving but not plotting is desired. Default is True
+        * save_spec : bool
+            If True, figure will be saved under **filename**. Default is
+            False
+        * new_fig : bool
+            If True, matplotlib will create a new fugure. Default is
+            True
+        * filename : str
+            filename of figure if saved. Default is "spectrogram.png"
+        * xlabel_rot : int or float
+            rotation angle (deg) of x-labels. Default is 70
+        * fmin : int or float
+            minimum frequency. Default is 0
+        * fmax : int or float
+            maximum frequency. Default is 32000
+        * vmin : int or float
+            lower limit of level axis (colormap). Default is 20
+        * vmax : int or float
+            upper limit of level axis (colormap). Default is 80
+        * figsize : (int, int)
+            width and height of figure, Default is (16, 9)
+    """
 
     # check for keys
     if 'plot_psd' not in kwargs: plot_psd = True
@@ -120,7 +226,7 @@ def plot_psd(psd_obj, **kwargs):
     if 'vmax' not in kwargs: vmax = 80.0
     if 'figsize' not in kwargs: figsize = (16,9)
 
-    #set backend for plotting/saving:
+    # set backend for plotting/saving:
     if not plot_psd: matplotlib.use('Agg')
     font = {'size'   : 22}
     matplotlib.rc('font', **font)
@@ -130,6 +236,7 @@ def plot_psd(psd_obj, **kwargs):
     else:
         f = psd_obj.freq
 
+    # plot PSD object
     if new_fig:
         fig, ax = plt.subplots(figsize=figsize)
     plt.semilogx(f, psd_obj.values)  
