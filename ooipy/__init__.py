3c1279afedcae65bb5cf27ab8faf8ed5bb39af38
FelixSchwock <fschwock@uw.edu>
2020-12-01 11:05:31 -0800

    added tools modules

--- ooipy/__init__.py
+++ ooipy/__init__.py
@@ -1,3 +1,5 @@
 from ooipy.hydrophone.basic import Psd, Spectrogram
 from ooipy.request.hydrophone_request import get_acoustic_data 
 from ooipy.request.hydrophone_request import get_acoustic_data_LF
+from ooipy.tools.ooiplotlib import plot
+import ooipy.tools.workflow
