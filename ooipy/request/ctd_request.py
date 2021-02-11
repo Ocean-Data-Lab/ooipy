d36217ad7962926fd87af8a13c8f93c0c5d22cb6
FelixSchwock <fschwock@uw.edu>
2021-02-10 16:26:28 -0800

    added delivery_method option to get_ctd_data

--- ooipy/request/ctd_request.py
+++ ooipy/request/ctd_request.py
@@ -7,7 +7,7 @@ from ooipy.ctd.basic import CtdData
 
 
 def get_ctd_data(start_datetime, end_datetime, location, limit=10000,
-                 only_profilers=False):
+                 only_profilers=False, delivery_method='auto'):
     """
     Requests CTD data between start_detetime and end_datetime for the
     specified location if data is available. For each location, data of
@@ -39,6 +39,21 @@ def get_ctd_data(start_datetime, end_datetime, location, limit=10000,
         of data points can contain more samples then indicated by limit
         if data from multiple CTDs is available at the given location
         and time. Default is 10,0000.
+    only_profilers : bool
+        Specifies whether only data from the water column profilers
+        should be requested. Default is False
+    delivery_method : str
+        Specifies which delivery method is considered. For details
+        please refer to http://oceanobservatories.org/glossary/. Options
+        are:
+        * 'auto' (default): automatically uses method that has data
+            avaiilable
+        * 'streamed': only consideres data that are streamed to shore
+            via cable
+        * 'telemetered': only consideres data that are streamed to shore
+            via satellite
+        * 'recovered': only consideres data that were reteived when the
+            instrument was retreived
 
     Returns
     -------
@@ -51,8 +66,6 @@ def get_ctd_data(start_datetime, end_datetime, location, limit=10000,
     # Sensor Inventory
     DATA_API_BASE_URL = \
         'https://ooinet.oceanobservatories.org/api/m2m/12576/sensor/inv/'
-
-    dataraw = []
 
     # Oregon Shelf
     if location == 'oregon_shelf':
@@ -88,13 +101,12 @@ def get_ctd_data(start_datetime, end_datetime, location, limit=10000,
              'dpc_ctd_instrument_recovered?',
              'RS01SBPD/DP01A/01-CTDPFL104/recovered_wfp/' +
              'dpc_ctd_instrument_recovered?',
-             'RS01SBPS/SF01A/2A-CTDPFA102/streamed/ctdpf_sbe43_sample?',]
+             'RS01SBPS/SF01A/2A-CTDPFA102/streamed/ctdpf_sbe43_sample?']
         if not only_profilers:
-            url_list.extend(['RS01SBPS/PC01A/4A-CTDPFA103/streamed/' + 
+            url_list.extend(['RS01SBPS/PC01A/4A-CTDPFA103/streamed/' +
                              'ctdpf_optode_sample?',
-                             'RS01SLBS/LJ01A/12-CTDPFB101/streamed/' + 
+                             'RS01SLBS/LJ01A/12-CTDPFB101/streamed/' +
                              'ctdpf_optode_sample?'])
-        
 
     elif location == 'oregon_inshore':
         url_list = \
@@ -184,10 +196,31 @@ def get_ctd_data(start_datetime, end_datetime, location, limit=10000,
     else:
         raise Exception('Do not know given location.')
 
+    # remove URLs if delivery method is specified
+    url_list_method = []
+    if (delivery_method == 'telemetered' or delivery_method == 'streamed'
+            or delivery_method == 'recovered'):
+        for url in url_list:
+            if delivery_method in url:
+                url_list_method.append(url)
+    elif delivery_method == 'auto':
+        url_list_method = url_list
+    else:
+        raise Exception("deliveriy method need to be either 'auto', " +
+                        "'telemetered', 'streamed', or 'recovered', but not " +
+                        str(delivery_method))
+
     beginDT = start_datetime.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + 'Z'
     endDT = end_datetime.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + 'Z'
 
-    for url in url_list:
+    dataraw = []
+    processed_ctds = []
+
+    for url in url_list_method:
+        ctd_id = __get_instrument_id(url)
+        if ctd_id in processed_ctds:
+            continue
+
         data_request_url = DATA_API_BASE_URL + url + \
             'beginDT=' + beginDT + '&endDT=' + endDT + '&limit=' + str(limit)
 
@@ -195,12 +228,20 @@ def get_ctd_data(start_datetime, end_datetime, location, limit=10000,
         dataraw2 = r.json()
         if 'message' not in dataraw2:
             dataraw.extend(dataraw2)
+            processed_ctds.append(ctd_id)
 
     return CtdData(raw_data=dataraw)
 
 
+def __get_instrument_id(url_str):
+    ctd_id_arr = url_str.split('/')[:3]
+    ctd_id = ctd_id_arr[0] + '/' + ctd_id_arr[1] + '/' + ctd_id_arr[2]
+
+    return ctd_id
+
+
 def get_ctd_data_daily(datetime_day, location, limit=10000,
-                       only_profilers=False):
+                       only_profilers=False, delivery_method='auto'):
     """
     Requests CTD data for specified day and location. The day is split
     into 24 1-hour periods and for each 1-hour period
@@ -213,21 +254,13 @@ def get_ctd_data_daily(datetime_day, location, limit=10000,
     datetime_day : datetime.datetime
         Day for which CTD data are requested
     location : str
-        location for which data are requested. Possible choices are:
-        * 'oregon_inshore'
-        * 'oregon_shelf'
-        * 'oregon_offshore'
-        * 'oregon_slope'
-        * 'washington_inshore'
-        * 'washington_shelf'
-        * 'washington_offshore'
-        * 'axial_base'
+        See :func:`ooipy.ctd_request.get_ctd_data
     limit : int
-        maximum number of data points returned in one request. The limit
-        applies for each instrument withing each 1-hour period
-        separately. That is the final list of data points likely
-        contains more samples then indicated by limit. Default is
-        10,0000.
+        See :func:`ooipy.ctd_request.get_ctd_data
+    only_profilers : bool
+        See :func:`ooipy.ctd_request.get_ctd_data
+    delivery_method : str
+        See :func:`ooipy.ctd_request.get_ctd_data
 
     Returns
     -------
@@ -247,9 +280,10 @@ def get_ctd_data_daily(datetime_day, location, limit=10000,
         end = datetime.datetime(year, month, day, hour, 59, 59, 999)
         start_end_list.append((start, end))
 
-    raw_data_arr = __map_concurrency(get_ctd_data_concurrent, start_end_list,
+    raw_data_arr = __map_concurrency(__get_ctd_data_concurrent, start_end_list,
                                      {'location': location, 'limit': limit,
-                                      'only_profilers': only_profilers})
+                                      'only_profilers': only_profilers,
+                                      'delivery_method': delivery_method})
 
     raw_data_falttened = []
     for item in raw_data_arr:
@@ -281,7 +315,8 @@ def __map_concurrency(func, iterator, args=(), max_workers=-1):
     return results
 
 
-def get_ctd_data_concurrent(start_end_tuple, location, limit, only_profilers):
+def __get_ctd_data_concurrent(start_end_tuple, location, limit, only_profilers,
+                              delivery_method):
     """
     Helper function to support multiprocessing for
     :func:`ooipy.ctd_request.get_ctd_data_daily
@@ -290,5 +325,6 @@ def get_ctd_data_concurrent(start_end_tuple, location, limit, only_profilers):
     end = start_end_tuple[1]
 
     rawdata = get_ctd_data(start, end, location=location, limit=limit,
-                           only_profilers=only_profilers)
+                           only_profilers=only_profilers,
+                           delivery_method=delivery_method)
     return rawdata
