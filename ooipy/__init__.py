2cc45e5b569e5372238934a8589a7460a315725c
Landung Setiawan <landungs@uw.edu>
2021-02-16 09:07:24 -0800

    Pull version in one line

--- ooipy/__init__.py
+++ ooipy/__init__.py
@@ -7,6 +7,4 @@ from ooipy.request.authentification import set_authentification
 from ooipy.tools.ooiplotlib import plot
 import ooipy.tools.workflow
 
-from ooipy._version import version
-
-__version__ = version
+from _version import version as __version__
