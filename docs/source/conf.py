d447c2234d2ba7adc1b6bd8f6b90c05c38eb2e7a
John Ragland <jhrag@uw.edu>
2021-03-29 14:07:00 -0700

    type fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ release = '1.0.4'
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage','sphinx.ext.napoleon']
 
-autodoc_mock_imports = ['numpy', 'matplotlib', 'obspy', 'scipy', 'pandas', 'thredds_crawler', 'lxml', 'requests', 'seaborn', 'fsspec', '_ooipy_versiongi']
+autodoc_mock_imports = ['numpy', 'matplotlib', 'obspy', 'scipy', 'pandas', 'thredds_crawler', 'lxml', 'requests', 'seaborn', 'fsspec', '_ooipy_version']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
