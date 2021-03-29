d2a36667547af8d1391ef35d7507e5e15aafa62d
John Ragland <jhrag@uw.edu>
2021-03-29 14:17:05 -0700

    added bullets to hydrophone node list

--- ooipy/request/hydrophone_request.py
+++ ooipy/request/hydrophone_request.py
@@ -9,6 +9,35 @@ in the Hydrophone Nodes section below.
         For a demo of the hydrophone request module, see the `Hydrophone
         Request Jupyter Notebook <_static/test_request.html>`_.
 
+Hydrophone Nodes
+^^^^^^^^^^^^^^^^
+* `Oregon Shelf Base Seafloor (Fs = 64 kHz) <https://ooinet.oceanobservatories.org/data_access/?search=CE02SHBP-LJ01D-11-HYDBBA106>`_
+    * 'LJ01D'
+* `Oregon Slope Base Seafloor (Fs = 64 kHz) <https://ooinet.oceanobservatories.org/data_access/?search=RS01SLBS-LJ01A-09-HYDBBA102>`_
+    * 'LJ01A'
+* `Slope Base Shallow (Fs = 64 kHz) <https://ooinet.oceanobservatories.org/data_access/?search=RS01SBPS-PC01A-08-HYDBBA103>`_
+    * 'PC01A'
+* `Axial Base Shallow Profiler (Fs = 64 kHz) <https://ooinet.oceanobservatories.org/data_access/?search=RS03AXPS-PC03A-08-HYDBBA303>`_
+    * 'PC03A'
+* `Offshore Base Seafloor (Fs = 64 kHz) <https://ooinet.oceanobservatories.org/data_access/?search=CE04OSBP-LJ01C-11-HYDBBA105>`_
+    * 'LJ01C'
+* `Axial Base Seafloor (Fs = 64 kHz) <https://ooinet.oceanobservatories.org/data_access/?search=RS03AXBS-LJ03A-09-HYDBBA302>`_
+    * 'LJ03A'
+* `Axial Base Seaflor (Fs = 200 Hz) <https://ooinet.oceanobservatories.org/data_access/?search=RS03AXBS-MJ03A-05-HYDLFA301>`_
+    * 'Axial_Base'
+    * 'AXABA1'
+* `Central Caldera (Fs = 200 Hz) <https://ooinet.oceanobservatories.org/data_access/?search=RS03CCAL-MJ03F-06-HYDLFA305>`_
+    * 'Central_Caldera'
+    * 'AXCC1'
+* `Eastern Caldera (Fs = 200 Hz) <https://ooinet.oceanobservatories.org/data_access/?search=RS03ECAL-MJ03E-09-HYDLFA304>`_
+    * 'Eastern_Caldera'
+    * 'AXEC2'
+* `Southern Hydrate (Fs = 200 Hz) <https://ooinet.oceanobservatories.org/data_access/?search=RS01SUM1-LJ01B-05-HYDLFA104>`_
+    * 'Southern_Hydrate'
+    * 'HYS14'
+* `Oregon Slope Base Seafloor (Fs = 200 Hz) <https://ooinet.oceanobservatories.org/data_access/?search=RS01SLBS-MJ01A-05-HYDLFA101>`_
+    * 'Slope_Base'
+    * 'HYSB1'
 '''
 
 # Import all dependancies
