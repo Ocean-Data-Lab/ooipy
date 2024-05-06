2eb8d97b9a95d7432c5b6c044eaf887360ac0951
John Ragland <jhrag@uw.edu>
2024-05-06 23:41:46 +0000

    updated tests to handle masked arrays

--- tests/test_request.py
+++ tests/test_request.py
@@ -20,8 +20,8 @@ def test_get_acoustic_data():
 
     data = hyd_request.get_acoustic_data(start_time, end_time, node)
 
-    assert type(data) is HydrophoneData
-    assert type(data.data) is np.ndarray
+    assert isinstance(data, HydrophoneData)
+    assert isinstance(data.data, (np.ndarray, np.ma.core.MaskedArray))
 
     diff_start = abs((start_time - data.stats.starttime.datetime).microseconds)
     diff_end = abs((end_time - data.stats.endtime.datetime).microseconds)
@@ -45,8 +45,8 @@ def test_get_acoustic_data():
 
     data = hyd_request.get_acoustic_data(start_time, end_time, node, append=False)
 
-    assert type(data) is HydrophoneData
-    assert type(data.data) is np.ndarray
+    assert isinstance(data, HydrophoneData)
+    assert isinstance(data.data, (np.ndarray, np.ma.core.MaskedArray))
 
     diff_start = abs((start_time - data.stats.starttime.datetime).microseconds)
     diff_end = abs((end_time - data.stats.endtime.datetime).microseconds)
