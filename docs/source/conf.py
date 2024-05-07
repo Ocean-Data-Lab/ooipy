5dd1ddccc8131646c289b746ead5b33a0677bbd3
John Ragland <jhrag@uw.edu>
2024-05-07 00:10:53 +0000

    updated mock dependancies for autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,7 @@ autodoc_mock_imports = [
     "seaborn",
     "fsspec",
     "_ooipy_version",
+    "xarray"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -61,7 +62,7 @@ master_doc = "index"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
