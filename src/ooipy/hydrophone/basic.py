455642fca3d46b371b5ce1e92484ad285e343ea5
John Ragland <jhrag@uw.edu>
2024-11-06 10:57:17 -0800

    Revert "removed legacy spec/psd objects"
    This reverts commit 9104482b580d4034b5dcddd1aedc5928010255e3.

--- src/ooipy/hydrophone/basic.py
+++ src/ooipy/hydrophone/basic.py
@@ -776,3 +776,322 @@ def _psd_mp_helper(ooi_hyd_data_obj, win, L, overlap, avg_method, interpolate, s
     """
     ooi_hyd_data_obj.compute_psd_welch(win, L, overlap, avg_method, interpolate, scale)
     return ooi_hyd_data_obj.psd
+
+
+class Spectrogram:
+    """
+    A class used to represent a spectrogram object.
+
+    Attributes
+    ----------
+    time : 1-D array of float or datetime objects
+        Indices of time-bins of spectrogram.
+    freq : 1-D array of float
+        Indices of frequency-bins of spectrogram.
+    values : 2-D array of float
+        Values of the spectrogram. For each time-frequency-bin pair there has
+        to be one entry in values. That is, if time has  length N and freq
+        length M, values is a NxM array.
+
+    """
+
+    def __init__(self, time, freq, values):
+        self.time = time
+        self.freq = freq
+        self.values = values
+
+    def visualize(
+        self,
+        plot_spec=True,
+        save_spec=False,
+        filename="spectrogram.png",
+        title="spectrogram",
+        xlabel="time",
+        xlabel_rot=70,
+        ylabel="frequency",
+        fmin=0,
+        fmax=32000,
+        vmin=20,
+        vmax=80,
+        vdelta=1.0,
+        vdelta_cbar=5,
+        figsize=(16, 9),
+        dpi=96,
+        res_reduction_time=1,
+        res_reduction_freq=1,
+        time_limits=None,
+    ):
+        """
+        This function will be depreciated into a different module in the future.
+        The current documentation might not be accurate.
+
+        To plot spectrograms please see
+        :meth:`ooipy.hydrophone.basic.plot_spectrogram`
+
+        Basic visualization of spectrogram based on matplotlib. The function
+        offers two options: Plot spectrogram in Python (plot_spec = True) and
+        save spectrogram plot in directory (save_spec = True). Spectrograms are
+        plotted in dB re 1µ Pa^2/Hz.
+
+        plot_spec (bool): whether or not spectrogram is plotted using Python
+        save_spec (bool): whether or not spectrogram plot is saved
+        filename (str): directory where spectrogram plot is saved. Use ending
+        ".png" or ".pdf" to save as PNG or PDF file. This value will be ignored
+        if save_spec=False
+        title (str): title of plot
+        ylabel (str): label of vertical axis
+        xlabel (str): label of horizontal axis
+        xlabel_rot (float): rotation of xlabel. This is useful if xlabel are
+        longer strings for example when using datetime.datetime objects.
+        fmin (float): minimum frequency (unit same as f) that is displayed
+        fmax (float): maximum frequency (unit same as f) that is displayed
+        vmin (float): minimum value (dB) of spectrogram that is colored.
+        All values below are displayed in white.
+        vmax (float): maximum value (dB) of spectrogram that is colored.
+        All values above are displayed in white.
+        vdelta (float): color resolution
+        vdelta_cbar (int): label ticks in colorbar are in vdelta_cbar steps
+        figsize (tuple(int)): size of figure
+        dpi (int): dots per inch
+        time_limits : list
+            specifies xlimits on spectrogram. List contains two
+            datetime.datetime objects
+        """
+        import warnings
+
+        raise warnings.warn(
+            "will be deprecated in future. Please see " "ooipy.tools.ooiplotlib.plot_spectrogram()"
+        )
+        # set backend for plotting/saving:
+        if not plot_spec:
+            matplotlib.use("Agg")
+
+        font = {"size": 22}
+        matplotlib.rc("font", **font)
+
+        v = self.values[::res_reduction_time, ::res_reduction_freq]
+
+        if len(self.time) != len(self.values):
+            t = np.linspace(
+                0,
+                len(self.values) - 1,
+                int(len(self.values) / res_reduction_time),
+            )
+        else:
+            t = self.time[::res_reduction_time]
+
+        # Convert t to np.array of datetime.datetime
+        if type(t[0]) is UTCDateTime:
+            for k in range(len(t)):
+                t[k] = t[k].datetime
+
+        if len(self.freq) != len(self.values[0]):
+            f = np.linspace(
+                0,
+                len(self.values[0]) - 1,
+                int(len(self.values[0]) / res_reduction_freq),
+            )
+        else:
+            f = self.freq[::res_reduction_freq]
+
+        cbarticks = np.arange(vmin, vmax + vdelta, vdelta)
+        fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
+        im = ax.contourf(
+            t,
+            f,
+            np.transpose(v),
+            cbarticks,
+            norm=colors.Normalize(vmin=vmin, vmax=vmax),
+            cmap=plt.cm.jet,
+        )
+        plt.ylabel(ylabel)
+        plt.xlabel(xlabel)
+        plt.ylim([fmin, fmax])
+        if time_limits is not None:
+            plt.xlim(time_limits)
+        plt.xticks(rotation=xlabel_rot)
+        plt.title(title)
+        plt.colorbar(im, ax=ax, ticks=np.arange(vmin, vmax + vdelta, vdelta_cbar))
+        plt.tick_params(axis="y")
+
+        if type(t[0]) is datetime.datetime:
+            ax.xaxis.set_major_formatter(mdates.DateFormatter("%y-%m-%d %H:%M"))
+
+        if save_spec:
+            plt.savefig(filename, dpi=dpi, bbox_inches="tight")
+
+        if plot_spec:
+            plt.show()
+        else:
+            plt.close(fig)
+
+    def save(self, filename="spectrogram.pickle"):
+        """
+        !!!!! This function will be moved into a different module in the
+        future. The current documentation might not be accurate !!!!!
+
+        Save spectrogram in pickle file.
+
+        filename (str): directory where spectrogram data is saved. Ending has
+        to be ".pickle".
+        """
+
+        dct = {"t": self.time, "f": self.freq, "spectrogram": self.values}
+        with open(filename, "wb") as outfile:
+            pickle.dump(dct, outfile)
+
+    def plot(self, **kwargs):
+        """
+        redirects to ooipy.ooiplotlib.plot_spectrogram()
+        please see :meth:`ooipy.hydrophone.basic.plot_spectrogram`
+        """
+        ooipy.tools.ooiplotlib.plot_spectrogram(self, **kwargs)
+
+
+class Psd:
+    """
+    A class used to represent a PSD object
+
+    Attributes
+    ----------
+    freq : array of float
+        Indices of frequency-bins of PSD.
+    values : array of float
+        Values of the PSD.
+
+    TODO:
+    Methods
+    -------
+    visualize(plot_psd=True, save_psd=False, filename='psd.png', title='PSD',
+    xlabel='frequency', xlabel_rot=0, ylabel='spectral level', fmin=0,
+    fmax=32, vmin=20, vmax=80, figsize=(16,9), dpi=96)
+        Visualizes PSD estimate using matplotlib.
+    save(filename='psd.json', ancillary_data=[], ancillary_data_label=[])
+        Saves PSD estimate and ancillary data in .json file.
+    """
+
+    def __init__(self, freq, values):
+        self.freq = freq
+        self.values = values
+
+    def visualize(
+        self,
+        plot_psd=True,
+        save_psd=False,
+        filename="psd.png",
+        title="PSD",
+        xlabel="frequency",
+        xlabel_rot=0,
+        ylabel="spectral level",
+        fmin=0,
+        fmax=32000,
+        vmin=20,
+        vmax=80,
+        figsize=(16, 9),
+        dpi=96,
+    ):
+        """
+        !!!!! This function will be moved into a different module in the
+        future. The current documentation might not be accurate !!!!!
+
+        Basic visualization of PSD estimate based on matplotlib. The function
+        offers two options: Plot PSD in Python (plot_psd = True) and save PSD
+        plot in directory (save_psd = True). PSDs are plotted in dB re 1µ
+        Pa^2/Hz.
+
+        plot_psd (bool): whether or not PSD is plotted using Python
+        save_psd (bool): whether or not PSD plot is saved
+        filename (str): directory where PSD plot is saved. Use ending ".png"
+        or ".pdf" to save as PNG or PDF
+            file. This value will be ignored if save_psd=False
+        title (str): title of plot
+        ylabel (str): label of vertical axis
+        xlabel (str): label of horizontal axis
+        xlabel_rot (float): rotation of xlabel. This is useful if xlabel are
+        longer strings.
+        fmin (float): minimum frequency (unit same as f) that is displayed
+        fmax (float): maximum frequency (unit same as f) that is displayed
+        vmin (float): minimum value (dB) of PSD.
+        vmax (float): maximum value (dB) of PSD.
+        figsize (tuple(int)): size of figure
+        dpi (int): dots per inch
+        """
+        import warnings
+
+        raise warnings.warn(
+            "will be deprecated in future. Please see " "ooipy.tools.ooiplotlib.plot_psd()"
+        )
+        # set backend for plotting/saving:
+        if not plot_psd:
+            matplotlib.use("Agg")
+
+        font = {"size": 22}
+        matplotlib.rc("font", **font)
+
+        if len(self.freq) != len(self.values):
+            f = np.linspace(0, len(self.values) - 1, len(self.values))
+        else:
+            f = self.freq
+
+        fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
+        plt.semilogx(f, self.values)
+        plt.ylabel(ylabel)
+        plt.xlabel(xlabel)
+        plt.xlim([fmin, fmax])
+        plt.ylim([vmin, vmax])
+        plt.xticks(rotation=xlabel_rot)
+        plt.title(title)
+        plt.grid(True)
+
+        if save_psd:
+            plt.savefig(filename, dpi=dpi, bbox_inches="tight")
+
+        if plot_psd:
+            plt.show()
+        else:
+            plt.close(fig)
+
+    def save(self, filename="psd.json", ancillary_data=[], ancillary_data_label=[]):
+        """
+        !!!!! This function will be moved into a different module in the
+        future. The current documentation might not be accurate !!!!!
+
+        Save PSD estimates along with with ancillary data
+        (stored in dictionary) in json file.
+
+        filename (str): directory for saving the data
+        ancillary_data ([array like]): list of ancillary data
+        ancillary_data_label ([str]): labels for ancillary data used as keys
+        in the output dictionary.
+            Array has same length as ancillary_data array.
+        """
+
+        if len(self.freq) != len(self.values):
+            f = np.linspace(0, len(self.values) - 1, len(self.values))
+        else:
+            f = self.freq
+
+        if type(self.values) is not list:
+            values = self.values.tolist()
+
+        if type(f) is not list:
+            f = f.tolist()
+
+        dct = {"psd": values, "f": f}
+
+        if len(ancillary_data) != 0:
+            for i in range(len(ancillary_data)):
+                if type(ancillary_data[i]) is not list:
+                    dct[ancillary_data_label[i]] = ancillary_data[i].tolist()
+                else:
+                    dct[ancillary_data_label[i]] = ancillary_data[i]
+
+        with open(filename, "w+") as outfile:
+            json.dump(dct, outfile)
+
+    def plot(self, **kwargs):
+        """
+        redirects to ooipy.ooiplotlib.plot_psd()
+        please see :meth:`ooipy.hydrophone.basic.plot_psd`
+        """
+        ooipy.tools.ooiplotlib.plot_psd(self, **kwargs)
