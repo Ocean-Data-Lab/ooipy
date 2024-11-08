fb3c79704e476c528ae72bd5bbdc3f08301cd742
John Ragland <john@Johns-MacBook-Pro.local>
2024-11-08 12:12:21 -0800

    updated __map_concurrency

--- src/ooipy/request/hydrophone_request.py
+++ src/ooipy/request/hydrophone_request.py
@@ -75,12 +75,6 @@ def get_acoustic_data(
         and end in case of boundary gaps in data. Default is True
     verbose : bool, optional
         specifies whether print statements should occur or not
-    data_gap_mode : int, optional
-        How gaps in the raw data will be handled. Options are:
-        '0': gaps will be linearly interpolated
-        '1': no interpolation; mask array is returned
-        '2': subtract mean of data and fill gap with zeros; mask array
-        is returned
     mseed_file_limit: int, optional
         If the number of mseed traces to be merged exceed this value, the
         function returns None. For some days the mseed files contain
@@ -271,7 +265,9 @@ def get_acoustic_data(
         __read_mseed, valid_data_url_list, verbose=verbose, max_workers=max_workers
     )
 
+    st_list_new = []
     # combine traces from single files into one trace if gapless merge is set to true
+    # if a single 5 minute file is is not compatible with gapless merge, it is currently removed
     if gapless_merge:
         for k, st in enumerate(st_list):
             # check if multiple traces in stream
@@ -286,8 +282,8 @@ def get_acoustic_data(
             # if valid npts, merge traces w/o consideration to gaps
             if npts_total / sampling_rate in [
                 300,
-                299.999,
-                300.001,
+            #    299.999,
+            #    300.001,
             ]:  # must be 5 minutes of samples
                 # NOTE it appears that npts_total is nondeterminstically off by ± 64 samples. I have
                 #   idea why, but am catching this here. Unknown what downstream effects this could
@@ -304,15 +300,26 @@ def get_acoustic_data(
                 stats["starttime"] = UTCDateTime(valid_data_url_list[k][-33:-6])
                 stats["endtime"] = UTCDateTime(stats["starttime"] + timedelta(minutes=5))
                 stats["npts"] = len(data_cat)
-
-                st_list[k] = Stream(traces=Trace(data_cat, header=stats))
+                st_list_new.append(Stream(traces=Trace(data_cat, header=stats)))
             else:
                 if verbose:
                     print(
                         f"Data segment {valid_data_url_list[k]}, \
                             with npts {npts_total}, is not compatible with gapless merge"
                     )
-                _ = st_list.pop(k)
+
+                # check if start times contain unique values
+                start_times = []
+                for tr in st_list[k]:
+                    start_times.append(tr.stats.starttime.strftime("%Y-%m-%dT%H:%M:%S"))
+                un_starttimes = set(start_times)
+                if len(un_starttimes) == len(st_list[k]):
+                    if verbose: print('file fragmented but timestamps are unique. Segment kept')
+                    st_list_new.append(st_list[k])
+                else:
+                    if verbose: print('file fragmented and timestamps are corrupt. Segment thrown out')
+                    pass
+        st_list = st_list_new
 
     # check if number of traces in st_list exceeds limit
     if mseed_file_limit is not None:
@@ -597,19 +604,19 @@ def __map_concurrency(func, iterator, args=(), max_workers=-1, verbose=False):
     if max_workers == -1:
         max_workers = 2 * mp.cpu_count()
 
-    results = []
+    results = [None] * len(iterator)
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-        # Start the load operations and mark each future with its URL
-        future_to_url = {executor.submit(func, i, *args): i for i in iterator}
+        # Start the load operations and mark each future with its index
+        future_to_index = {executor.submit(func, i, *args): idx for idx, i in enumerate(iterator)}
         # Disable progress bar
         is_disabled = not verbose
         for future in tqdm(
-            concurrent.futures.as_completed(future_to_url),
+            concurrent.futures.as_completed(future_to_index),
             total=len(iterator),
             disable=is_disabled,
         ):
-            data = future.result()
-            results.append(data)
+            idx = future_to_index[future]
+            results[idx] = future.result()
     return results
 
 
