d0c4e4d20f94af443784cb30ff661f31f0013844
John Ragland <jhrag@uw.edu>
2025-03-02 07:29:04 -0800

    re-added pytest

--- tests/test_request.py
+++ tests/test_request.py
@@ -80,47 +80,31 @@ def test_get_acoustic_data_LF():
     assert type(hdata.data) is np.ndarray
 
 
-# Temporarily removing test on hydrophone names. Axial_Base_Shallow
-# doesn't seem to have data for specified time region
+def test_hydrophone_node_names():
+   node_arr = [
+       "Oregon_Shelf_Base_Seafloor",
+       "Oregon_Slope_Base_Seafloor",
+       "Oregon_Slope_Base_Shallow",
+       "Axial_Base_Shallow",
+       "Oregon_Offshore_Base_Seafloor",
+       "Axial_Base_Seafloor",
+   ]
+   node_id_arr = ["LJ01D", "LJ01A", "PC01A", "PC03A", "LJ01C", "LJ03A"]
+   starttime = datetime.datetime(2017, 3, 20, 0, 0, 0)  # time of first sample
+   endtime = datetime.datetime(2017, 3, 20, 0, 0, 1)  # time of last sample
+   for item in node_arr:
+       hyd_data = hyd_request.get_acoustic_data(starttime, endtime,
+node=item, gapless_merge=False)
+       assert hyd_data.stats.location in node_id_arr
+   node_arr = [
+       "Slope_Base",
+       "Southern_Hydrate",
+       "Axial_Base",
+       "Central_Caldera",
+       "Eastern_Caldera",
+   ]
+   node_id_arr = ["HYSB1", "HYS14", "AXBA1", "AXCC1", "AXEC2"]
+   for item in node_arr:
+       hyd_data = hyd_request.get_acoustic_data_LF(starttime, endtime, node=item)
+       assert hyd_data.stats.location in node_id_arr
 
-# def test_hydrophone_node_names():
-#    node_arr = [
-#        "Oregon_Shelf_Base_Seafloor",
-#        "Oregon_Slope_Base_Seafloor",
-#        "Oregon_Slope_Base_Shallow",
-#        "Axial_Base_Shallow",
-#        "Oregon_Offshore_Base_Seafloor",
-#        "Axial_Base_Seafloor",
-#    ]
-#    node_id_arr = ["LJ01D", "LJ01A", "PC01A", "PC03A", "LJ01C", "LJ03A"]
-#
-#    starttime = datetime.datetime(2017, 3, 20, 0, 0, 0)  # time of first sample
-#    endtime = datetime.datetime(2017, 3, 20, 0, 0, 1)  # time of last sample
-#
-#    for item in node_arr:
-#        hyd_data = hyd_request.get_acoustic_data(starttime, endtime,
-# node=item, gapless_merge=False)
-#        assert hyd_data.stats.location in node_id_arr
-#
-#    node_arr = [
-#        "Slope_Base",
-#        "Southern_Hydrate",
-#        "Axial_Base",
-#        "Central_Caldera",
-#        "Eastern_Caldera",
-#    ]
-#    node_id_arr = ["HYSB1", "HYS14", "AXBA1", "AXCC1", "AXEC2"]
-#
-#    for item in node_arr:
-#        hyd_data = hyd_request.get_acoustic_data_LF(starttime, endtime, node=item)
-#        assert hyd_data.stats.location in node_id_arr
-
-
-# TODO: need to figure out how to do the OOI authentification on the GitHub VM
-# def test_get_ctd_data():
-#    start = datetime.datetime(2016, 12, 17, 2, 0, 0)
-#    end = datetime.datetime(2016, 12, 17, 3, 0, 0)
-#    ctd_data = ctd_request.get_ctd_data(start, end, 'oregon_offshore', limit=120)
-#
-#    assert type(ctd_data) == HydrophoneData
-#    assert len(ctd_data.raw_data) > 0
